50da771972ba780bb404bbbdc62dcd9f644e9936
Kurnia Ramadhan Putra <kurniaramadhanp91@gmail.com>
2015-10-28 23:20:25 +0700

    All commit

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,51 +1,31 @@
 (function() {
-
-  'use strict';
-  var express = require('express');
-  var router = express.Router();
-  var mongojs = require('mongojs');
-  var db = mongojs('mongodb://admin:admin123@ds063809.mongolab.com:63809/meantodo', ['todos']);
-
-  /* GET home page. */
-  router.get('/', function(req, res) {
-    res.render('index');
-  });
-
-  router.get('/api/todos', function(req, res) {
-    db.todos.find(function(err, data) {
-      res.json(data);
+    'use strict';
+    var express = require('express');
+    var router = express.Router();
+    var db = require('diskdb');
+    db = db.connect('server/db', ['todos']);
+    /* GET home page. */
+    router.get('/', function(req, res) {
+        res.render('index');
     });
-  });
-
-  router.post('/api/todos', function(req, res) {
-    db.todos.insert(req.body, function(err, data) {
-      res.json(data);
+    router.get('/api/todos', function(req, res) {
+        res.json(db.todos.find());
     });
-
-  });
-
-  router.put('/api/todos', function(req, res) {
-
-    db.todos.update({
-      _id: mongojs.ObjectId(req.body._id)
-    }, {
-      isCompleted: req.body.isCompleted,
-      todo: req.body.todo
-    }, {}, function(err, data) {
-      res.json(data);
+    router.post('/api/todos', function(req, res) {
+        res.json(db.todos.save(req.body));
     });
-
-  });
-
-  router.delete('/api/todos/:_id', function(req, res) {
-    db.todos.remove({
-      _id: mongojs.ObjectId(req.params._id)
-    }, '', function(err, data) {
-      res.json(data);
+    router.put('/api/todos', function(req, res) {
+        res.json(db.todos.update({
+            _id: req.body._id
+        }, {
+            isCompleted: req.body.isCompleted,
+            todo: req.body.todo
+        }));
     });
-
-  });
-
-  module.exports = router;
-
+    router.delete('/api/todos/:_id', function(req, res) {
+        res.json(db.todos.remove({
+            _id: req.params._id
+        }));
+    });
+    module.exports = router;
 }());
